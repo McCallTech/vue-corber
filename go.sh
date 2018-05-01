eceef2a3e566c2d2a367890826f93f1c79f302df
JoshMcCall <joshmccall221@gmail.com>
2018-05-01 13:55:54 -0500

    go

---
+++ go.sh
@@ -0,0 +1,52 @@
+sudo apt-get -y install software-properties-common
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get -y install java-common oracle-java8-installer
+sudo apt-get -y install oracle-java8-set-default
+source /etc/profile
+
+export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+export PATH=$JAVA_HOME/bin:$PATH
+
+sudo add-apt-repository ppa:cwchien/gradle
+sudo apt-get update
+sudo apt-get -y install gradle
+sudo add-apt-repository ppa:maarten-fonville/android-studio
+sudo apt-get update
+sudo apt-get -y install android-studio
+
+
+
+# download android sdk
+wget https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
+sudo apt-get install unzip
+mkdir -p ~/Android/Sdk/
+unzip sdk-tools-linux-3859397.zip -d ~/Android/Sdk/
+
+export ANDROID_HOME="/home/$(whoami)/Android/Sdk/"
+export PATH="${ANDROID_HOME}tools/bin/:${ANDROID_HOME}tools/:${ANDROID_HOME}platform-tools/:${PATH}";
+
+# install all sdk packages
+##android update sdk --no-ui
+sdkmanager --update
+sdkmanager "platform-tools" "build-tools;26.0.3"  "platforms;android-26"
+ls ${ANDROID_HOME}
+
+sudo apt-get -y install bzip2
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+. ~/.nvm/nvm.sh; nvm i --lts;
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+##yarn install
+vue init webpack vue
+cd vue
+git init; git add .; git commit -m "inital"
+sed -i "s%'/'%''%g" ./config/index.js
+touch vue.config.js
+npm i -g yarn ember-cli corber cordova http-server
+rm -rf corber
+corber init
+corber platform remove android
+corber platform add android
+corber proxy requirements
+corber build --platform=android
+http-server -p 3000 ./corber/cordova/platforms/android/app/build/outputs/apk/debug/
